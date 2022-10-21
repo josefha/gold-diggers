20ea976e2767d428df76baf2cac98e89404e03bc
Robin Ellgren <robin.ellgren@mindler.se>
2022-10-21 14:53:36 +0200

    Add price

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* App.js */
 import React, { Component } from 'react';
 import CanvasJSReact from './canvasjs.react';
-// var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 class App extends Component {
 	render() {
@@ -10,16 +10,33 @@ class App extends Component {
     const numberOfLayers = 20;
     const dataPoints = [];
     const startY = 2850;
+    const startMoney = 1;
     const currentSales = ["Robin","Hugo","Shani","Victor","David"];
     let Y = startY;
+    let oldPrice = startMoney;
     for(var i=0; i<numberOfLayers; i++){
       const yForThis = Y*1;
       const label = i<currentSales.length ? currentSales[i] : "Open for sale"
-      dataPoints.push({label, y: yForThis});
+      const price = oldPrice*2;
+      dataPoints.push({label, y: yForThis, price });
+      oldPrice=price;
       Y = yForThis;
     }
     const reversedData = dataPoints.reverse();
+    const colors = [];
+    const gold= "#D4AF37";
+    const gray ="#C8C6C4";
+    for(var i=0; i<dataPoints.length; i++){
+      if(i < currentSales.length){
+        colors.push(gold);
+      }else{
+        colors.push(gray);
+      }
+    }
+    const reversedColors = colors.reverse();
+    CanvasJS.addColorSet("goldAndGray",reversedColors);
 		const options = {
+      colorSet: "goldAndGray",
       height: 1000,
 			animationEnabled: true,
 			title: {
@@ -28,7 +45,7 @@ class App extends Component {
 			data: [{
 				type: "pyramid",
 				legendText: "{label}",
-				indexLabel: "{label} - {y}",
+				indexLabel: "{label} - {price}$",
 				toolTipContent: "<b>{label}</b>: {y} <b>({percentage}%)</b>",
 				dataPoints: reversedData
 			}]
