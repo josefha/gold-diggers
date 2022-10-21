970bed0703f93efb7911b455754cdb0b323cd87c
Robin Ellgren <robin.ellgren@mindler.se>
2022-10-21 14:26:42 +0200

    Add triangle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* App.js */
+import React, { Component } from 'react';
+import CanvasJSReact from './canvasjs.react';
+// var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+class App extends Component {
+	render() {
+		var dataPoint;
+		var total;
+		const options = {
+			animationEnabled: true,
+			title: {
+				text: "Sales via Advertisement"
+			},
+			legend: {
+				horizontalAlign: "right",
+				verticalAlign: "center",
+				reversed: true
+			},
+			data: [{
+				type: "pyramid",
+				showInLegend: true,
+				legendText: "{label}",
+				indexLabel: "{label} - {y}",
+				toolTipContent: "<b>{label}</b>: {y} <b>({percentage}%)</b>",
+				dataPoints: [
+					{ label: "Impressions", y: 2850},
+					{ label: "Clicked", y: 2150},
+					{ label: "Free Downloads", y: 1900},
+					{ label: "Purchase", y: 650},
+					{ label: "Renewal", y: 250}
+				]
+			}]
+		}
+		//calculate percentage
+		dataPoint = options.data[0].dataPoints;
+		total = dataPoint[0].y;
+		for(var i = 0; i < dataPoint.length; i++) {
+			if(i === 0) {
+				options.data[0].dataPoints[i].percentage = 100;
+			} else {
+				options.data[0].dataPoints[i].percentage = ((dataPoint[i].y / total) * 100).toFixed(2);
+			}
+		}
+		return (
+		<div>
+			<CanvasJSChart options = {options}
+				 /*onRef={ref => this.chart = ref}*/
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+		);
+	}
 }
-
 export default App;
